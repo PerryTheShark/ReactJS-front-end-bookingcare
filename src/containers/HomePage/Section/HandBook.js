c915b3feb987aa9aeebcac1c5014509a465a0f70
PeryTheShark <tungtv.uet@gmail.com>
2023-04-27 16:43:48 +0700

    #50 react

--- src/containers/HomePage/Section/HandBook.js
+++ src/containers/HomePage/Section/HandBook.js
@@ -1,42 +1,41 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
-
 import Slider from "react-slick";
 
-class Specialty extends Component {
+class HandleBook extends Component {
   render() {
     return (
-      <div className="section-share section-specialty">
+      <div className="section-share section-handbook">
         <div className="section-container">
           <div className="section-header">
-            <span className="title-section">Chuyen khoa pho bien</span>
+            <span className="title-section">Cam nang</span>
             <button className="btn-section">xem them</button>
           </div>
           <div className="section-body">
             <Slider {...this.props.settings}>
               <div className="section-customize">
-                <div className="bg-image section-specialty" />
-                <div>Co xuong khop 1</div>
+                <div className="bg-image section-handbook" />
+                <div>Xuong khop 1</div>
               </div>
               <div className="section-customize">
-                <div className="bg-image section-specialty" />
-                <div>Co xuong khop 2</div>
+                <div className="bg-image section-handbook" />
+                <div>Xuong khop 2</div>
               </div>
               <div className="section-customize">
-                <div className="bg-image section-specialty" />
-                <div>Co xuong khop 3</div>
+                <div className="bg-image section-handbook" />
+                <div>Xuong khop 3</div>
               </div>
               <div className="section-customize">
-                <div className="bg-image section-specialty" />
-                <div>Co xuong khop 4</div>
+                <div className="bg-image section-handbook" />
+                <div>Xuong khop 4</div>
               </div>
               <div className="section-customize">
-                <div className="bg-image section-specialty" />
-                <div>Co xuong khop 5</div>
+                <div className="bg-image section-handbook" />
+                <div>Xuong khop 5</div>
               </div>
               <div className="section-customize">
-                <div className="bg-image section-specialty" />
-                <div>Co xuong khop 6</div>
+                <div className="bg-image section-handbook" />
+                <div>Xuong khop 6</div>
               </div>
             </Slider>
           </div>
@@ -49,8 +48,11 @@ class Specialty extends Component {
 const mapStateToProps = (state) => {
   return {
     isLoggedIn: state.user.isLoggedIn,
-    language: state.app.language,
   };
 };
 
-export default connect(mapStateToProps)(Specialty);
+const mapDispatchToProps = (dispatch) => {
+  return {};
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(HandleBook);
