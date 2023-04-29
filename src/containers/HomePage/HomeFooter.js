cde3a96d07f74a504abf2b5c2b399f2419820cdc
PeryTheShark <tungtv.uet@gmail.com>
2023-04-29 16:31:52 +0700

    finish frontend homepage

---
+++ src/containers/HomePage/HomeFooter.js
@@ -0,0 +1,31 @@
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { FormatedMessage } from "react-intl";
+
+class HomeFooter extends Component {
+  render() {
+    return (
+      <div className="home-footer">
+        <p>
+          &copy; 2023 PerryTheShark own this. More information, visit my github.
+          <a target="_blank" href="https://github.com/PerryTheShark">
+            &#8594; Click here &#8592;
+          </a>
+        </p>
+      </div>
+    );
+  }
+}
+
+const mapStateToProps = (state) => {
+  return {
+    isLoggedIn: state.user.isLoggedIn,
+    language: state.app.language,
+  };
+};
+
+const mapDispatchToProps = (dispatch) => {
+  return {};
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(HomeFooter);
