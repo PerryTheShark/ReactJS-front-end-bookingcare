13ea64016e8cdb02867050ecf95711e0ea2d0461
PeryTheShark <tungtv.uet@gmail.com>
2023-05-12 13:53:44 +0700

    add form full input and load data, add input images

--- src/containers/System/Admin/UserRedux.js
+++ src/containers/System/Admin/UserRedux.js
@@ -1,31 +1,28 @@
 import React, { Component } from "react";
 import { FormattedMessage } from "react-intl";
 import { connect } from "react-redux";
-import { getAllCodeService } from "../../../services/userService";
 import { LANGUAGES } from "../../../utils";
 import * as actions from "../../../store/actions";
+import "./UserRedux.scss";
+import Lightbox from "react-image-lightbox";
+import "react-image-lightbox/style.css";
 
 class UserRedux extends Component {
   constructor(props) {
     super(props);
     this.state = {
       genderArr: [],
+      positionArr: [],
+      roleArr: [],
+      previewImgURL: "",
+      isOpen: false,
     };
   }
 
   async componentDidMount() {
     this.props.getGenderStart();
-    // try {
-    //   let res = await getAllCodeService("gender");
-    //   if (res && res.errCode === 0) {
-    //     this.setState({
-    //       genderArr: res.data,
-    //     });
-    //   }
-    //   console.log("hoidanit check res: ", res);
-    // } catch (error) {
-    //   console.log(error);
-    // }
+    this.props.getPositionStart();
+    this.props.getRoleStart();
   }
 
   componentDidUpdate(prevProps, prevState, snapshot) {
@@ -34,14 +31,49 @@ class UserRedux extends Component {
         genderArr: this.props.genderRedux,
       });
     }
+
+    if (prevProps.positionRedux !== this.props.positionRedux) {
+      this.setState({
+        positionArr: this.props.positionRedux,
+      });
+    }
+
+    if (prevProps.roleRedux !== this.props.roleRedux) {
+      this.setState({
+        roleArr: this.props.roleRedux,
+      });
+    }
   }
+
+  handleOnchangeImage = (event) => {
+    let data = event.target.files;
+    let file = data[0];
+    if (file) {
+      const objectUrl = URL.createObjectURL(file);
+      this.setState({
+        previewImgURL: objectUrl,
+      });
+    }
+  };
+
+  openPreviewImage = () => {
+    if (!this.state.previewImgURL) return;
+    this.setState({
+      isOpen: true,
+    });
+  };
 
   render() {
     let genders = this.state.genderArr;
+    let roles = this.state.roleArr;
+    let positions = this.state.positionArr;
+
     let language = this.props.language;
+    let isGetGenders = this.props.isLoadingGender;
     return (
       <div className="user-redux-container">
         <div className="title">Learn React-Redux with PerryTheShark</div>
+        <div>{isGetGenders === true ? "Loading Gender" : ""}</div>
         <div className="user-redux-body">
           <div className="container">
             <div className="row">
@@ -107,8 +139,17 @@ class UserRedux extends Component {
                   <FormattedMessage id="manage-user.position" />
                 </label>
                 <select className="form-control">
-                  <option selected>Choose...</option>
-                  <option>...</option>
+                  {positions &&
+                    positions.length > 0 &&
+                    positions.map((item, index) => {
+                      return (
+                        <option key={index}>
+                          {language === LANGUAGES.VI
+                            ? item.valueVi
+                            : item.valueEn}
+                        </option>
+                      );
+                    })}
                 </select>
               </div>
               <div className="col-3">
@@ -116,15 +157,43 @@ class UserRedux extends Component {
                   <FormattedMessage id="manage-user.role" />
                 </label>
                 <select className="form-control">
-                  <option selected>Choose...</option>
-                  <option>...</option>
+                  {roles &&
+                    roles.length > 0 &&
+                    roles.map((item, index) => {
+                      return (
+                        <option key={index}>
+                          {language === LANGUAGES.VI
+                            ? item.valueVi
+                            : item.valueEn}
+                        </option>
+                      );
+                    })}
                 </select>
               </div>
               <div className="col-3">
                 <label>
                   <FormattedMessage id="manage-user.image" />
                 </label>
-                <input type="text" className="form-control" />
+                <div className="preview-img-container">
+                  <input
+                    id="previewImg"
+                    type="file"
+                    hidden
+                    onChange={(event) => this.handleOnchangeImage(event)}
+                  />
+                  <label className="label-upload" htmlFor="previewImg">
+                    Tải ảnh <i className="fas fa-upload"></i>
+                  </label>
+                  <div
+                    className="preview-image"
+                    style={{
+                      backgroundImage: `url(${this.state.previewImgURL})`,
+                    }}
+                    onClick={() => this.openPreviewImage()}
+                  >
+                    {" "}
+                  </div>
+                </div>
               </div>
               <div className="col-12">
                 <button className="btn btn-primary">
@@ -134,7 +203,12 @@ class UserRedux extends Component {
             </div>
           </div>
         </div>
-        ;
+        {this.state.isOpen === true && (
+          <Lightbox
+            mainSrc={this.state.previewImgURL}
+            onCloseRequest={() => this.setState({ isOpen: false })}
+          />
+        )}
       </div>
     );
   }
@@ -144,12 +218,19 @@ const mapStateToProps = (state) => {
   return {
     language: state.app.language,
     genderRedux: state.admin.genders,
+    positionRedux: state.admin.positions,
+    roleRedux: state.admin.roles,
+    isLoadingGender: state.admin.isLoadingGender,
   };
 };
 
 const mapDispatchToProps = (dispatch) => {
   return {
     getGenderStart: () => dispatch(actions.fetchGenderStart()),
+
+    getPositionStart: () => dispatch(actions.fetchPositionStart()),
+
+    getRoleStart: () => dispatch(actions.fetchRoleStart()),
     // processLogout: () => dispatch(actions.processLogout()),
     // changeLanguageAppRedux: (language) =>
     //   dispatch(actions.changeLanguageApp(language)),
--- src/store/actions/adminActions.js
+++ src/store/actions/adminActions.js
@@ -8,9 +8,9 @@ import { getAllCodeService } from "../../services/userService";
 export const fetchGenderStart = () => {
   return async (dispatch, getState) => {
     try {
+      dispatch({ type: actionTypes.FETCH_GENDER_START });
       let res = await getAllCodeService("GENDER");
       if (res && res.errCode === 0) {
-        console.log("hoiPerryTheShark check get state: ", getState);
         dispatch(fetchGenderSuccess(res.data));
       } else {
         dispatch(fetchGenderFailed());
@@ -31,4 +31,53 @@ export const fetchGenderFailed = () => ({
   type: actionTypes.FETCH_GENDER_FAILED,
 });
 
+export const fetchPositionStart = () => {
+  return async (dispatch, getState) => {
+    try {
+      let res = await getAllCodeService("POSITION");
+      if (res && res.errCode === 0) {
+        dispatch(fetchPositionSuccess(res.data));
+      } else {
+        dispatch(fetchPositionFailed());
+      }
+    } catch (error) {
+      dispatch(fetchPositionFailed());
+      console.log("fetchPositionFaild", error);
+    }
+  };
+};
+
+export const fetchPositionSuccess = (positionData) => ({
+  type: actionTypes.FETCH_POSITION_SUCCESS,
+  data: positionData,
+});
+
+export const fetchPositionFailed = () => ({
+  type: actionTypes.FETCH_POSITION_FAILED,
+});
+
+export const fetchRoleStart = () => {
+  return async (dispatch, getState) => {
+    try {
+      let res = await getAllCodeService("ROLE");
+      if (res && res.errCode === 0) {
+        dispatch(fetchRoleSuccess(res.data));
+      } else {
+        dispatch(fetchRoleFailed());
+      }
+    } catch (error) {
+      dispatch(fetchRoleFailed());
+      console.log("fetchRoleFaild", error);
+    }
+  };
+};
+
+export const fetchRoleSuccess = (roleData) => ({
+  type: actionTypes.FETCH_ROLE_SUCCESS,
+  data: roleData,
+});
+
+export const fetchRoleFailed = () => ({
+  type: actionTypes.FETCH_ROLE_FAILED,
+});
 //start -> doing -> end;
