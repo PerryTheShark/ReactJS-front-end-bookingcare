de9f9e2105f1b8ad61c2901809f5996868319ad3
PeryTheShark <tungtv.uet@gmail.com>
2023-06-07 09:43:50 +0700

    add admin create new specialty

--- src/containers/System/Specialty/ManageSpecialty.js
+++ src/containers/System/Specialty/ManageSpecialty.js
@@ -1,16 +1,24 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
 import { FormattedMessage } from "react-intl";
+import { CommonUtils } from "../../../utils";
 import "./ManageSpecialty.scss";
 import MarkdownIt from "markdown-it";
 import MdEditor from "react-markdown-editor-lite";
+import { createNewSpecialty } from "../../../services/userService";
+import { toast } from "react-toastify";
 
 const mdParser = new MarkdownIt(/* Markdown-it options */);
 
 class ManageSpecialty extends Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      name: "",
+      imageBase64: "",
+      descriptionHTML: "",
+      descriptionMarkdown: "",
+    };
   }
 
   async componentDidMount() {}
@@ -20,6 +28,43 @@ class ManageSpecialty extends Component {
     }
   }
 
+  handleOnchangeInput = (event, id) => {
+    let stateCopy = { ...this.state };
+    stateCopy[id] = event.target.value;
+    this.setState({
+      ...stateCopy,
+    });
+  };
+
+  handleEditorChange = ({ html, text }) => {
+    this.setState({
+      descriptionMarkdown: text,
+      descriptionHTML: html,
+    });
+  };
+
+  handleOnchangeImage = async (event) => {
+    let data = event.target.files;
+    let file = data[0];
+    if (file) {
+      let base64 = await CommonUtils.getBase64(file);
+
+      this.setState({
+        imageBase64: base64,
+      });
+    }
+  };
+
+  handleSaveNewSpecialty = async () => {
+    let res = await createNewSpecialty(this.state);
+    if (res && res.errCode === 0) {
+      toast.success("Add new specialty succeeds!");
+    } else {
+      toast.error("something went wrong....");
+      console.log("Response error: ", res);
+    }
+  };
+
   render() {
     return (
       <div className="manage-specialty-container">
@@ -28,22 +73,36 @@ class ManageSpecialty extends Component {
         <div className="add-new-specialty row">
           <div className="col-6 form-group">
             <label>Ten chuyen khoa</label>
-            <input className="form-control"></input>
+            <input
+              className="form-control"
+              type="text"
+              value={this.state.name}
+              onChange={(event) => this.handleOnchangeInput(event, "name")}
+            ></input>
           </div>
           <div className="col-6 form-group">
             <label>Image specialty</label>
-            <input className="form-control-file" type="file"></input>
+            <input
+              className="form-control-file"
+              type="file"
+              onChange={(event) => this.handleOnchangeImage(event)}
+            ></input>
           </div>
           <div className="col-12">
             <MdEditor
-              style={{ height: "500px" }}
+              style={{ height: "300px" }}
               renderHTML={(text) => mdParser.render(text)}
-              // onChange={this.handleEditorChange}
-              // value={this.state.contentMarkdown}
+              onChange={this.handleEditorChange}
+              value={this.state.descriptionMarkdown}
             />
           </div>
           <div className="col-12">
-            <button className="btn-save-specialty">Save</button>
+            <button
+              className="btn-save-specialty"
+              onClick={() => this.handleSaveNewSpecialty()}
+            >
+              Save
+            </button>
           </div>
         </div>
       </div>
